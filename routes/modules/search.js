7485c21eec355d62d3c2471707518883f1a22886
Pin Hsu Chen <paul791111@gmail.com>
2022-05-17 23:27:00 +0800

    Feat: modify search function

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -5,24 +5,23 @@ const Restaurants = require('../../models/restaurant')
 // Search restaurant
 router.get('/', (req, res) => {
   if (!req.query.keyword) res.redirect('/')
-  const keyword = req.query.keyword.trim().toLowerCase()
+  const keyword =  new RegExp(req.query.keyword.trim(), 'i')
   const sortKey = req.query.sort || 'name'
 
-  Restaurants.find()
+  Restaurants.find({
+    $or: [
+      {name: {$regex: keyword}},
+      {category: {$regex: keyword}}
+    ] 
+  })
     .lean()
     .sort(sortKey)
-    .then(restaurant => {
-      const filteredRestaurant = restaurant.filter(restaurants => 
-        restaurants.name.toLowerCase().includes(keyword) || restaurants.category.toLowerCase().includes(keyword)
-      )
-      return filteredRestaurant
-    })
     .then(filteredRestaurant => {
-      if (!filteredRestaurant.length) {
+      if (!filteredRestaurant) {
         req.flash('error_messages', `沒有符合 ${keyword} 的餐廳或類別`)
         res.redirect('/')
       } else {
-        res.render('index', { restaurant: filteredRestaurant, keyword })  
+        res.render('index', { restaurant: filteredRestaurant, keyword: req.query.keyword })  
       }
     })
     .catch(error => console.error(error))
