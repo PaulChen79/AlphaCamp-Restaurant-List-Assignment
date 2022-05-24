c2ee7a2ed5c5512127250b621f9360dea354d71b
Pin Hsu Chen <paul791111@gmail.com>
2022-05-24 10:55:43 +0800

    Feat: add facebook login

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const exphbs = require('express-handlebars')
 const routes = require('./routes/index')
 const session = require('express-session')
 const flash = require('connect-flash')
 const mtehodOverride = require('method-override')
+const usePassport = require('./config/passport')
 const app = express()
 require('./config/mongoose')
-require('dotenv').config()
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -14,16 +15,18 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(mtehodOverride('_method'))
-app.use(session({ secret: 'SECRET', resave: false, saveUninitialized: false }))
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }))
+usePassport(app)
 app.use(flash())
 app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages')
-  res.locals.error_messages = req.flash('error_messages')
-  next()
+	res.locals.success_messages = req.flash('success_messages')
+	res.locals.error_messages = req.flash('error_messages')
+	next()
 })
 
 app.use(routes)
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+	console.log('Server is running on port 3000')
 })
+
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const home = require('./modules/home')
 const search = require('./modules/search')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
 
-router.use('/', home)
-router.use('/search', search)
 router.use('/restaurants', restaurants)
 router.use('/users', users)
+router.use('/search', search)
+router.use('/auth', auth)
+router.use('/', home)
 
 module.exports = router
 
